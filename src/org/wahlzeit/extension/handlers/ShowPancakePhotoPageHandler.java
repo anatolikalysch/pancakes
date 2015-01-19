0062bd46226d8cb85ecf905a29c56d67a0616b18
kalysch <kalysch.anatoli@googlemail.com>
2015-01-19 14:39:25 +0100

    ShowPhotoHandler updated; Exceptions for Location and Pancakes updated;

--- src/org/wahlzeit/extension/handlers/ShowPancakePhotoPageHandler.java
+++ src/org/wahlzeit/extension/handlers/ShowPancakePhotoPageHandler.java
@@ -3,13 +3,17 @@
  */
 package org.wahlzeit.extension.handlers;
 
+import org.wahlzeit.extension.location.LocationArgumentException;
+import org.wahlzeit.extension.model.PancakeArgumentException;
 import org.wahlzeit.extension.model.PancakePhoto;
+import org.wahlzeit.extension.model.userDialog.ExtendedUserSession;
 import org.wahlzeit.extension.utils.ExtendedHtmlUtil;
 import org.wahlzeit.handlers.PartUtil;
 import org.wahlzeit.handlers.ShowPhotoPageHandler;
 import org.wahlzeit.model.AccessRights;
 import org.wahlzeit.model.Photo;
 import org.wahlzeit.model.UserSession;
+import org.wahlzeit.services.SysLog;
 import org.wahlzeit.webparts.WebPart;
 
 /**
@@ -34,19 +38,36 @@ public class ShowPancakePhotoPageHandler extends ShowPhotoPageHandler {
 	protected void makePhotoCaption(UserSession us, WebPart page) {
 		PancakePhoto photo = (PancakePhoto) us.getPhoto();
 		// String photoId = photo.getId().asString();
+		ExtendedUserSession eus = new ExtendedUserSession(us);
 			
 		WebPart caption = createWebPart(us, PartUtil.CAPTION_INFO_FILE);
 		caption.addString(Photo.CAPTION, getPhotoCaption(us, photo));
 		// pass over the domain data to be shown in the caption
 		PancakePhoto temp = (PancakePhoto) photo;
-		caption.addString("pancakeId", temp.getPancake().getIdAsString());
-		caption.addString("name", temp.getPancake().getType().getName());
-		caption.addString("recipe", temp.getPancake().getType().getRecipe().toString());
-		// pass over ingredients as table
-		caption.addString("ingredients", ExtendedHtmlUtil.asIngredientTable(temp.getPancake()));
-		// pass over the location data to be shown in the caption
-		caption.addString("location", temp.getLocation().toString());
-		
+		try {
+			caption.addString("pancakeId", temp.getPancake().getIdAsString());
+			caption.addString("name", temp.getPancake().getType().getName());
+			caption.addString("recipe", temp.getPancake().getType().getRecipe().toString());
+			// pass over ingredients as table
+			caption.addString("ingredients", ExtendedHtmlUtil.asIngredientTable(temp.getPancake()));
+			// pass over the location data to be shown in the caption
+			caption.addString("location", temp.getLocation().toString());
+		} catch (PancakeArgumentException e) {
+			SysLog.logThrowable(e);
+			us.setMessage((eus.cfg()).getPancakeIllegalArguments(e.getMessage()));
+		} catch (LocationArgumentException e) {
+			SysLog.logThrowable(e);
+			us.setMessage((eus.cfg()).getLocationIllegalArguments(e.getMessage()));
+		} catch (IllegalArgumentException e) {
+			SysLog.logThrowable(e);
+			us.setMessage(eus.cfg().getPancakeIllegalArguments(e.getMessage()));
+		} catch (AssertionError e) { 
+			SysLog.logThrowable(e);
+			us.setMessage(eus.cfg().getPancakePostViolation(e.getMessage()));
+		} catch (Exception omega) { //something beyond my control happened, so photoupload failed
+			SysLog.logThrowable(omega);
+			us.setMessage(eus.cfg().getPhotoUploadFailed());
+		}
 		// write caption
 		page.addWritable(Photo.CAPTION, caption);
 	}
--- src/org/wahlzeit/extension/model/PancakeArgumentException.java
+++ src/org/wahlzeit/extension/model/PancakeArgumentException.java
@@ -3,22 +3,18 @@ package org.wahlzeit.extension.model;
 public class PancakeArgumentException extends IllegalArgumentException {
 
 	public PancakeArgumentException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public PancakeArgumentException(String s) {
 		super(s);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PancakeArgumentException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public PancakeArgumentException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
